c97b91f1329f0d83b2828e3f103fd0478405e22f
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-10-16 18:30:56 +0200

    axios call and card project component, done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
 <script>
+import axios from 'axios';
+import ProjectCard from "./components/ProjectCard.vue";
+
 export default {
-  data() {}
+  components: {
+    ProjectCard,
+  },
+  data() {
+    return {
+      projects: [],
+      pagination: {}
+    }
+  },
+  methods: {
+    fetchData() {
+      axios.get('http://127.0.0.1:8000/api/projects')
+        .then((response) => {
+          // salviamo la lista dei progetti
+          this.projects = response.data.data;
+
+          // cancello il data da response.data.data
+          delete response.data.data;
+
+          // salvo la paginazione dei miei progetti
+          this.pagination = response.data;
+          console.log(response);
+        })
+    }
+  },
+  mounted() {
+    this.fetchData();
+  }
 }
 </script>
 
 <template>
-  <div class="container">
-    <h1>ciao</h1>
-  </div>
+  <main>
+    <div class="container py-5">
+      <h1 class="text-center text-uppercase pb-5">I miei progetti</h1>
+
+      <div class="row row-cols-3 g-3">
+        <div class="col" v-for="project in projects" :key="project.id">
+          <ProjectCard :project="project" />
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
+
+<style lang="scss"></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    props: {
+        project: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        getImageUrl(project) {
+            return `http://127.0.0.1:8000/storage/${project.image}`
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <img :src="getImageUrl(project)" class="card-img-top" alt="image not found">
+        <div class="card-body">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <small class="card-text fw-bold">
+                {{ project.type.name }}
+            </small>
+            <div>
+                <div 
+                    v-for="technology in project.technologies" 
+                    :key="technology.id" 
+                    class="badge me-2"
+                    :style="`background-color: ${technology.color}`">
+                        {{ technology.name }}
+                </div>
+            </div>
+            <p class="card-text">{{ project.description }}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+img {
+    min-height: 208.5px;
+    object-fit: cover;
+    object-position: top left;
+}
+</style>
